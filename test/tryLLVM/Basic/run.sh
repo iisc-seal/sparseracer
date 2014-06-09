c1b5802907f28066759d7362ddc00a4a0ccffdc1
Anirudh Santhiar <anirudh_s@csa.iisc.ernet.in>
2014-06-09 22:32:07 +0530

    changed run.sh to reflect environment

--- test/tryLLVM/Basic/run.sh
+++ test/tryLLVM/Basic/run.sh
@@ -1,11 +1,11 @@
-export PATH=/home/anirudh/software/llvm/build/bin:$PATH
+export PATH=/mnt/hdd2/anirudh/software/llvm-ff/build/bin:$PATH
 #clang mop.c -c -o mop.o
 #clang allocDalloc.c -c -o allocDalloc.o
 # clang++ -O0 -Xclang -load -Xclang /home/anirudh/software/llvm/build/lib/LLVMLoadStoreInstrument.so -Xclang -load -Xclang /home/anirudh/software/llvm/build/lib/LLVMAllocFreeInstrument.so -Xclang -load -Xclang /home/anirudh/software/llvm/build/lib/LLVMFInstrument.so -g struct3.cpp -c -o struct3.o
 # clang++ mop.o allocDalloc.o struct3.o -o struct3
-clang++ -O0 -Xclang -load -Xclang /home/anirudh/software/llvm/build/lib/LLVMMemInstrument.so -g struct2.cpp -c -o struct2.o
+clang++ -O0 -Xclang -load -Xclang /mnt/hdd2/anirudh/software/llvm-ff/build/lib/LLVMMemInstrument.so -g struct2.cpp -c -o struct2.o
 #clang++ mop.o allocDalloc.o struct3.o -o struct3
-clang++ struct2.o -o struct2 -L/home/anirudh -L/home/anirudh/software/firefoxSources/firefox-29.0/instrument -linstrument
+clang++ struct2.o -o struct2 -L/home/anirudh -L/mnt/hdd2/anirudh/software/firefoxBuilds/firefox-29.0/instrument -linstrument
 
 #-emit-llvm -S struct3.cpp -o struct3.bc
 # opt -load /home/anirudh/software/llvm/build/lib/LLVMLoadStoreInstrument.so -offline -load /home/anirudh/software/llvm/build/lib/LLVMAllocFreeInstrument.so -logAllocDealloc < struct3.bc > struct3.instrument.bc
